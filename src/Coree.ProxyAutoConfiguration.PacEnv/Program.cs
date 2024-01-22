2ee93f043483687eecb75d17e4bad778cd6cb2b9
Carsten Riedel <carsten.riedel.one@outlook.com>
2024-01-22 21:08:38 +0100

    refactor

--- src/Coree.ProxyAutoConfiguration.PacEnv/Program.cs
+++ src/Coree.ProxyAutoConfiguration.PacEnv/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using Coree.ProxyAutoConfiguration.Library;
+using NLog;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
@@ -20,12 +22,21 @@ namespace Coree.ProxyAutoConfiguration.PacEnv
 
         internal sealed class PacEnvCommand : Command<PacEnvCommand.Settings>
         {
+            public enum Scope
+            {
+                User,
+                Process,
+            }
+
             public sealed class Settings : CommandSettings
             {
-                
                 [Description("Specifies the path to the .pac file. Accepts local file paths and URLs (http, https, ftp). Requires direct network access. For Windows, leaving this empty defaults to registry settings. On other operating systems, this argument is mandatory.")]
                 [CommandArgument(0, "[ProxyAutoConfigurationLocation]")]
                 public string? PacLocation { get; init; }
+
+                [CommandOption("-s|--scope")]
+                [Description("Sets the scope for the proxy environment variables. 'user' applies the settings to the current user profile, while 'process' applies them only to the current process. Default is 'process'.")]
+                public Scope Scope { get; init; } = Scope.Process;
 
                 [Description("Specifies the URL for which to find the appropriate proxy settings. This option allows you to determine the proxy configuration for a specific web resource. By default, it is set to 'https://example.com'.")]
                 [DefaultValue("https://example.com")]
@@ -83,8 +94,6 @@ namespace Coree.ProxyAutoConfiguration.PacEnv
                     AnsiConsole.Markup($"[red]Failed to read PAC script: {errorMessage}.[/]");
                     return -2;
                 }
-                
-
 
                 // Assuming FullJavaScript is a combination of PAC script and necessary functions
                 var FullJavaScript = contentResult.Content + Environment.NewLine + JintInvoke.MozillaPacFunctions;
@@ -102,22 +111,27 @@ namespace Coree.ProxyAutoConfiguration.PacEnv
                         // Green color for success
                         AnsiConsole.Markup("[green]Proxy script configuration value:[/] -> " + FindProxyForURLResult.ReturnValue);
                         break;
+
                     case JintScriptRunner.ExecutionState.FunctionNonInvokable:
                         // Red color for script error
                         AnsiConsole.Markup("[red]Error: There was an error in the PAC script.[/]");
                         return -1;
+
                     case JintScriptRunner.ExecutionState.ScriptMissing:
                         // Red color for no script found
                         AnsiConsole.Markup("[red]Error: No PAC script was found.[/]");
                         return -1;
+
                     case JintScriptRunner.ExecutionState.FunctionNotFound:
                         // Red color for function not found
                         AnsiConsole.Markup("[red]Error: The FindProxyForURL function was not found in the PAC script.[/]");
                         return -1;
+
                     case JintScriptRunner.ExecutionState.ScriptExecutionError:
                         // Red color for execution error
                         AnsiConsole.Markup("[red]Error: There was an execution error: " + FindProxyForURLResult.Exception?.Message + "[/]");
                         return -1;
+
                     default:
                         // Red color for unknown errors
                         AnsiConsole.Markup("[red]Error: An unknown error occurred.[/]");
@@ -147,7 +161,6 @@ namespace Coree.ProxyAutoConfiguration.PacEnv
                     return -1;
                 }
 
-
                 FindProxyForURLResultParser.ProxyConfiguration FirstProxy = entries.First(e => e.Type == FindProxyForURLResultParser.ProxyType.Proxy);
 
                 FirstProxy.ReturnValue = "http://" + FirstProxy.ReturnValue;
@@ -156,16 +169,38 @@ namespace Coree.ProxyAutoConfiguration.PacEnv
                 // Process the PAC script content if needed
                 // ...
 
-                System.Environment.SetEnvironmentVariable("HTTP_PROXY", FirstProxy.ReturnValue, EnvironmentVariableTarget.Process);
-                System.Environment.SetEnvironmentVariable("HTTPS_PROXY", FirstProxy.ReturnValue, EnvironmentVariableTarget.Process);
+                switch (settings.Scope)
+                {
+                    case Scope.User:
+                        System.Environment.SetEnvironmentVariable("HTTP_PROXY", FirstProxy.ReturnValue, EnvironmentVariableTarget.User);
+                        System.Environment.SetEnvironmentVariable("HTTPS_PROXY", FirstProxy.ReturnValue, EnvironmentVariableTarget.User);
+                        break;
+                    case Scope.Process:
+                        System.Environment.SetEnvironmentVariable("HTTP_PROXY", FirstProxy.ReturnValue, EnvironmentVariableTarget.Process);
+                        System.Environment.SetEnvironmentVariable("HTTPS_PROXY", FirstProxy.ReturnValue, EnvironmentVariableTarget.Process);
+                        break;
+                    default:
+                        break;
+                }
 
-                System.Collections.IDictionary vars =  System.Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
+
+
+                return 0;
+            }
+
+            public void Dump()
+            {
+                var vars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process)
+            .Cast<DictionaryEntry>()
+            .Select(entry => new KeyValuePair<string, string>(
+                entry.Key?.ToString() ?? string.Empty,
+                entry.Value?.ToString() ?? string.Empty))
+            .ToList();
+
+                //var vars = System.Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process).Cast<DictionaryEntry>().Cast<KeyValuePair<string, string>>();
 
                 // Convert IDictionary to a List of KeyValuePairs and sort it by key
-                var sortedVars = vars.Cast<System.Collections.DictionaryEntry>()
-                                     .Select(entry => new KeyValuePair<string, string>(entry.Key.ToString(), entry.Value.ToString()))
-                                     .OrderBy(entry => entry.Key)
-                                     .ToList();
+                var sortedVars = vars.OrderBy(entry => entry.Key).ToList();
 
                 foreach (var var in sortedVars)
                 {
@@ -174,19 +209,7 @@ namespace Coree.ProxyAutoConfiguration.PacEnv
 
                     AnsiConsole.MarkupLine($"[blue]{keyText}[/], [green]{valueText}[/]");
                 }
-
-
-
-                return 0;
             }
-
-            public PacResolver Parse([NotNull] CommandContext context, [NotNull] Settings settings)
-            {
-
-
-                return pacResolver;
-            }
-
         }
     }
 }
--- src/Coree.ProxyAutoConfiguration.PacEnv/test.pac
+++ src/Coree.ProxyAutoConfiguration.PacEnv/test.pac
@@ -1,1 +1,7 @@
-﻿me
+﻿function FindProxyForURL(url, host) {
+    // This is a simple stub function for demonstration purposes.
+    // You can customize the logic based on your specific requirements.
+
+    // Example: Always use a specific proxy server
+    return "PROXY 192.168.1.100:8080; DIRECT ; FPX 1203.12";
+}
